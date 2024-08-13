11ca14b0b694e57b9a317a605e8e098ca2e9bb57
rubenssdhusdshdf <rubenvegajusto@gmail.com>
2024-08-13 21:21:24 +0100

    fibonacci

--- data-structures/Queues/Queue/src/main/java/ds5/EmptyQueueException.java
+++
@@ -1,8 +0,0 @@
-package ds5;
-
-public class EmptyQueueException extends RuntimeException {
-    public EmptyQueueException() {
-        super("There is nothing in the queue :)"); // super is used to calling the sueprclass Constructor or to access
-                                                   // superclass Methods or fields
-    }
-}
